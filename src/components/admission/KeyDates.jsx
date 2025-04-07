511585210a452a6721c5d2ed59bf3185fc775257
Praz <Praz@ALIPL5595.local>
2025-04-07 19:06:11 +0530

    Table fixed mob view

--- src/components/admission/KeyDates.jsx
+++ src/components/admission/KeyDates.jsx
@@ -5,12 +5,11 @@ import { admissionScreenData } from "@/constants/data";
 export const KeyDates = () => {
   return (
     <Box>
+      {/* Header Box */}
       <Box
         sx={{
-          borderTop: "1px solid #FB7F05",
-          borderRight: "1px solid #FB7F05",
-          borderLeft: "1px solid #FB7F05",
-          borderBottom: { xs: "1px solid #FB7F05", md: "none" },
+          border: "1px solid #FB7F05",
+          borderBottom: "none",
           borderTopLeftRadius: "16px",
           borderTopRightRadius: "16px",
           padding: "20px",
@@ -29,40 +28,34 @@ export const KeyDates = () => {
           {admissionScreenData.keyDates.headerText}
         </Typography>
       </Box>
-      <Box sx={{ display: "flex", flexDirection: { xs: "row", md: "column" } }}>
-        {/* First Row */}
+
+      {/* Desktop View */}
+      <Box sx={{ display: { xs: "none", md: "block" } }}>
+        {/* First Row - Header */}
         <Box
           sx={{
-            width: "100%",
-            padding: "20px",
             display: "flex",
-            flexDirection: { xs: "column", md: "row" },
             backgroundColor: "rgba(251, 127, 5, 0.3)",
-            gap: { xs: "2.5rem", sm: 0 },
-            borderBottomLeftRadius: { xs: "16px", md: 0 },
-            "& > *:nth-last-of-type(3)": {
-              marginBottom: { xs: "1rem", md: 0 }, // Applies only to the second last child
-            },
-            "& > *:nth-last-of-type(2)": {
-              marginBottom: { xs: "0.5rem", md: 0 }, // Applies only to the second last child
-            },
+            padding: "20px",
+            borderLeft: "1px solid #FB7F05",
+            borderRight: "1px solid #FB7F05",
           }}
         >
           {admissionScreenData.keyDates.firstArray.map((item, index) => (
             <Box
-              key={index}
+              key={`desktop-header-${index}`}
               sx={{
-                flex: { md: "1 0 20%" },
-                paddingLeft: { md: index === 0 ? 0 : "12px" },
+                flex: 1,
+                textAlign: "center",
+                paddingLeft: index !== 0 ? "12px" : 0,
                 paddingRight: "12px",
               }}
             >
               <Typography
                 sx={{
-                  fontWeight: "600",
+                  fontWeight: "500",
                   lineHeight: "100%",
                   color: "#1E1E1E",
-                  textAlign: "center",
                   fontFamily: "Inter",
                 }}
               >
@@ -72,28 +65,24 @@ export const KeyDates = () => {
           ))}
         </Box>
 
-        {/* Second Row */}
+        {/* Second Row - Content */}
         <Box
           sx={{
-            width: "100%",
-            padding: "20px",
             display: "flex",
-            flexDirection: { xs: "column", md: "row" },
-            borderLeft: { xs: "none", md: "1px solid #FB7F05" },
-            borderRight: "1px solid #FB7F05",
-            borderBottom: "1px solid #FB7F05",
+            padding: "20px",
+            border: "1px solid #FB7F05",
+            borderTop: "none",
+            borderBottomLeftRadius: "16px",
             borderBottomRightRadius: "16px",
-            borderBottomLeftRadius: { xs: "0", md: "16px" },
-            gap: { xs: "2rem", sm: 0 },
-            // justifyContent: { xs: "space-around", md: "" },
           }}
         >
           {admissionScreenData.keyDates.secondArray.map((item, index) => (
             <Box
-              key={index}
+              key={`desktop-content-${index}`}
               sx={{
-                flex: { md: "1 0 20%" },
-                paddingLeft: { md: index === 0 ? 0 : "12px" },
+                flex: 1,
+                textAlign: "center",
+                paddingLeft: index !== 0 ? "12px" : 0,
                 paddingRight: "12px",
               }}
             >
@@ -102,12 +91,77 @@ export const KeyDates = () => {
                   fontWeight: "500",
                   lineHeight: "150%",
                   color: "#1E1E1E",
-                  textAlign: "center",
                   fontFamily: "Inter",
                 }}
               >
                 {item}
               </Typography>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+
+      {/* Mobile View - Tabular Layout */}
+      <Box sx={{ display: { xs: "block", md: "none" } }}>
+        <Box
+          sx={{
+            borderTop: "1px solid #FB7F05",
+            borderRight: "1px solid #FB7F05",
+            borderBottom: "1px solid #FB7F05",
+            borderBottomLeftRadius: "16px",
+            borderBottomRightRadius: "16px",
+            overflow: "hidden",
+          }}
+        >
+          {admissionScreenData.keyDates.firstArray.map((header, index) => (
+            <Box
+              key={`mobile-row-${index}`}
+              sx={{
+                display: "flex",
+                // borderBottom: index === 4 ? "none" : "1px solid #FB7F05",
+              }}
+            >
+              {/* Header Cell */}
+              <Box
+                sx={{
+                  flex: 1,
+                  padding: "16px",
+                  backgroundColor: "rgba(251, 127, 5, 0.3)",
+                  // borderRight: "1px solid #FB7F05",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: "500",
+                    lineHeight: "150%",
+                    color: "#1E1E1E",
+                    fontFamily: "Inter",
+                    textAlign: "center",
+                  }}
+                >
+                  {header}
+                </Typography>
+              </Box>
+
+              {/* Value Cell */}
+              <Box
+                sx={{
+                  flex: 1,
+                  padding: "16px",
+                  textAlign: "center",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: "400",
+                    lineHeight: "150%",
+                    color: "#1E1E1E",
+                    fontFamily: "Inter",
+                  }}
+                >
+                  {admissionScreenData.keyDates.secondArray[index]}
+                </Typography>
+              </Box>
             </Box>
           ))}
         </Box>
