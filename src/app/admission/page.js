034bb3d77ea728f8575cabfff343ff0a4ee575f1
SubheshVedam <subhesh@vedam.org>
2025-04-17 13:46:29 +0530

    Subhesh_Testing2

--- src/app/admission/page.js
+++ src/app/admission/page.js
@@ -16,7 +16,7 @@ export const metadata = {
         url: '/img/Vedam_Final_Logo_White.png',
         width: 1200,
         height: 630,
-        alt: 'Admission & Fees - Vedam School of Technology',
+        alt: 'Admissions & Fees - Vedam School of Technology',
       },
     ],
   },
