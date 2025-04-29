188c090e48931e7d3aa9df20838e075d5ed055f1
SubheshVedam <subhesh@vedam.org>
2025-04-29 16:07:10 +0530

    Subhesh_adding_change_date

--- src/constants/data.js
+++ src/constants/data.js
@@ -642,7 +642,7 @@ export const admissionScreenData = {
     ],
     secondArray: [
       "12th May 2025",
-      "9th May 2025",
+      "11th May 2025",
       "Within 3 Days of the Exam",
       "Within 10 Days of the Exam",
       "Within 20 Days of the Exam, If selected",
@@ -664,7 +664,7 @@ export const admissionScreenData = {
         title: "Apply Online",
         subtitle:
           "Complete your profile and reserve your slot for the Vedam Scholastic Aptitude Test (VSAT).",
-        subtitle1: "Last Date to Register is 9th May 2025",
+        subtitle1: "Last Date to Register is 11th May 2025",
         showButton: true,
       },
       {
@@ -672,7 +672,7 @@ export const admissionScreenData = {
         step: "Step 2",
         title: "Take the VSAT",
         subtitle:
-          "It is an entrance test conducted online. May intake round on 9th May, 2025.",
+          "It is an entrance test conducted online. May intake round on 12th May, 2025.",
         showButton: false,
       },
       {
