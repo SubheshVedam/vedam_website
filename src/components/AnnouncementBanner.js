c0d0f3a5833020a9858c36186c1ae35468dfcbd3
SubheshVedam <subhesh@vedam.org>
2025-07-03 21:31:28 +0530

    Updating timer2

--- src/components/AnnouncementBanner.js
+++ src/components/AnnouncementBanner.js
@@ -3,7 +3,7 @@ import { Box, Typography, Stack } from "@mui/material";
 import { useState, useEffect } from "react";
 
 export default function AnnouncementBanner() {
-  const targetDate = new Date("2025-07-03T23:59:59").getTime();
+  const targetDate = new Date("2025-07-04T13:59:59").getTime();
 
   const [timeLeft, setTimeLeft] = useState(null);
 
