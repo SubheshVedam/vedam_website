e3497140fb96bfc9a6be4bab5336ffda3f503793
SubheshVedam <subhesh@vedam.org>
2025-07-11 02:50:26 +0530

    Updating Rewards

--- src/app/home/client/Home.jsx
+++ src/app/home/client/Home.jsx
@@ -21,6 +21,7 @@ import { homeScreenData } from "@/constants/data";
 import { Box, useMediaQuery, useTheme } from "@mui/material";
 import Image from "next/image";
 import React, { Fragment, useState } from "react";
+import RecognitionAwards from "@/components/home/RecognitionAwards";
 
 const Home = () => {
   const theme = useTheme();
@@ -266,6 +267,7 @@ const Home = () => {
             <WhatPeople />
           </CardContainer>
 
+          <RecognitionAwards />
           <CardContainer subtitle={homeScreenData.investorWhoTrustUs.subtitle}>
             <InvestorWhoTrustUs />
           </CardContainer>
