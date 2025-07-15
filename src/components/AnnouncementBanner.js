d55f4c6fae4d4e15cf9d5717f4cce39e3172754c
SubheshVedam <subhesh@vedam.org>
2025-07-15 17:48:09 +0530

    Update time for 28th

--- src/components/AnnouncementBanner.js
+++ src/components/AnnouncementBanner.js
@@ -3,7 +3,7 @@ import { Box, Typography, Stack } from "@mui/material";
 import { useState, useEffect } from "react";
 
 export default function AnnouncementBanner() {
-  const targetDate = new Date("2025-07-15T14:00:00").getTime();
+  const targetDate = new Date("2025-07-28T23:59:59").getTime();
 
   const [timeLeft, setTimeLeft] = useState(null);
 
