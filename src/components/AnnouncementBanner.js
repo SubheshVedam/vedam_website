f01bc03d312ee0c314dce8f3dacfe04484283c5d
SubheshVedam <subhesh@vedam.org>
2025-07-17 15:37:41 +0530

    Update banner

--- src/components/AnnouncementBanner.js
+++ src/components/AnnouncementBanner.js
@@ -62,7 +62,7 @@ export default function AnnouncementBanner() {
           variant="body2"
           component="span"
           sx={{ fontSize: { xs: 11, sm: 16 } }}>
-          ⏳ Hurry up! Applications for 29th July<strong>[Final Intake]</strong> are closing in
+          ⏳ Hurry up! Applications for 29th July <strong>[Final Intake]</strong> are closing in
         </Typography>
         {timeLeft === null ? null : !timeLeft.expired ? (
           <Typography
