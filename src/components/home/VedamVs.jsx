5ea60121f21779c9b0638c8f43ef5a41fa94ee2f
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-11-26 17:08:14 +0530

    VF-445 updated the mobile view of VedamVs component

--- src/components/home/VedamVs.jsx
+++ src/components/home/VedamVs.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+'use client'
+import React, { useRef } from "react";
 import Image from "next/image";
 import { homeScreenData } from "@/constants/data";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, IconButton } from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 const Card = ({
   data,
@@ -92,84 +95,268 @@ const Card = ({
 };
 
 export const VedamVs = () => {
+  const scrollContainerRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      const cardWidth = scrollContainerRef.current.offsetWidth;
+      scrollContainerRef.current.scrollBy({
+        left: -cardWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      const cardWidth = scrollContainerRef.current.offsetWidth;
+      scrollContainerRef.current.scrollBy({
+        left: cardWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <Box
-      sx={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        overflowX: { xs: "auto", md: "hidden" },
-        "&::-webkit-scrollbar": { display: "none" },
-        scrollbarWidth: "none",
-      }}
-    >
-      <Card
-        data={homeScreenData.vedamVs.leftSideData}
-        containerStyle={{
-          flex: { xs: "0 0 40%", md: "0 0 30%" },
-          dispaly: "flex",
-          alignItems: "flex-start",
-          marginTop: "1rem",
-        }}
-        showBorder
-        textArrayStyle={{
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "flex-start",
-        }}
-        textArrayContainer={{ width: "100%" }}
-        textArrayTextStyle={{ fontWeight: "700", color: "rgba(30, 30, 30, 1)" }}
-      />
-      <Box
+    <Box sx={{ position: "relative" }}>
+      {/* Left Scroll Button - Only visible on mobile */}
+      <IconButton
+        onClick={scrollLeft}
         sx={{
-          flex: { xs: "0 0 80%", md: "0 0 40%" },
-          paddingTop: "1rem",
-          paddingBottom: "1rem",
-          background:
-            "linear-gradient(160.16deg, #BA6BFF -29.47%, #6C10BC 39.76%, #1E1E1E 101.6%)",
-          borderRadius: "20px",
+          display: { xs: "flex", md: "none" },
+          position: "absolute",
+          left: 0,
+          top: "50%",
+          transform: "translateY(-50%)",
+          zIndex: 100,
+          opacity: 0.8,
+          backgroundColor: "white",
+          "&:hover": {
+            backgroundColor: "white",
+          },
         }}
       >
+        <ChevronLeftIcon />
+      </IconButton>
+
+      <Box
+        ref={scrollContainerRef}
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          overflowX: { xs: "auto", md: "hidden" },
+          "&::-webkit-scrollbar": { display: "none" },
+          scrollbarWidth: "none",
+          scrollSnapType: { xs: "x mandatory", md: "none" },
+        }}
+      >
+        {/* Mobile: Show left column + middle card combined */}
+        <Box
+          sx={{
+            display: { xs: "flex", md: "none" },
+            flex: "0 0 100%",
+            border: '1px solid #8A18FF',
+            borderRadius: "20px",
+            scrollSnapAlign: "start",
+            gap: "0",
+          }}
+        >
+          <Card
+            data={homeScreenData.vedamVs.leftSideData}
+            containerStyle={{
+              flex: "0 0 35%",
+              display: "flex",
+              alignItems: "flex-start",
+              marginTop: "1rem",
+            }}
+            showBorder
+            textArrayStyle={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+            }}
+            textArrayContainer={{ width: "100%" }}
+            textArrayTextStyle={{
+              fontWeight: "700",
+              color: "rgba(30, 30, 30, 1)",
+            }}
+          />
+          <Box
+            sx={{
+              flex: "0 0 65%",
+              paddingTop: "1rem",
+              paddingBottom: "1rem",
+              background:
+                "linear-gradient(160.16deg, #BA6BFF -29.47%, #6C10BC 39.76%, #1E1E1E 101.6%)",
+              borderRadius: "20px",
+            }}
+          >
+            <Card
+              width="100%"
+              data={homeScreenData.vedamVs.middleSideData}
+              containerStyle={{
+                paddingLeft: "28px",
+                paddingRight: "28px",
+                background:
+                  "linear-gradient(160.16deg, #BA6BFF -29.47%, #6C10BC 39.76%, #1E1E1E 101.6%)",
+                borderRadius: "20px",
+              }}
+              textArrayTextStyle={{
+                color: "rgba(255, 255, 255, 1)",
+                textAlign: "left",
+              }}
+            />
+          </Box>
+        </Box>
+
+        {/* Mobile: Show left column + right card combined */}
+        <Box
+          sx={{
+            display: { xs: "flex", md: "none" },
+            flex: "0 0 100%",
+            border: '1px solid #8A18FF',
+            borderRadius: "20px",
+            scrollSnapAlign: "start",
+            gap: "0",
+          }}
+        >
+          <Card
+            data={homeScreenData.vedamVs.leftSideData}
+            containerStyle={{
+              flex: "0 0 35%",
+              display: "flex",
+              alignItems: "flex-start",
+              marginTop: "1rem",
+            }}
+            showBorder
+            textArrayStyle={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+            }}
+            textArrayContainer={{ width: "100%" }}
+            textArrayTextStyle={{
+              fontWeight: "700",
+              color: "rgba(30, 30, 30, 1)",
+            }}
+          />
+          <Card
+            data={homeScreenData.vedamVs.rightSideData}
+            containerStyle={{
+              flex: "0 0 65%",
+              border: "1px solid rgba(108, 16, 188, 1)",
+              borderRadius: "20px",
+              height: "414px",
+              paddingTop: "35px",
+              paddingBottom: "20px",
+              // marginTop: "1rem",
+            }}
+            textArrayStyle={{
+              width: "100%",
+              paddingLeft: "20px",
+              justifyContent: "flex-start",
+            }}
+            headerTextStyle={{ color: "rgba(12, 38, 81, 1)" }}
+            textArrayTextStyle={{
+              color: "rgba(132, 132, 132, 1)",
+            }}
+          />
+        </Box>
+
+        {/* Desktop: Original layout */}
         <Card
-          width="100%"
-          data={homeScreenData.vedamVs.middleSideData}
+          data={homeScreenData.vedamVs.leftSideData}
           containerStyle={{
-            paddingLeft: { xs: "28px", md: "40px" },
-            paddingRight: { xs: "28px", md: "40px" },
+            display: { xs: "none", md: "flex" },
+            flex: "0 0 30%",
+            alignItems: "flex-start",
+            marginTop: "1rem",
+          }}
+          showBorder
+          textArrayStyle={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-start",
+          }}
+          textArrayContainer={{ width: "100%" }}
+          textArrayTextStyle={{ fontWeight: "700", color: "rgba(30, 30, 30, 1)" }}
+        />
+        <Box
+          sx={{
+            display: { xs: "none", md: "block" },
+            flex: "0 0 40%",
+            paddingTop: "1rem",
+            paddingBottom: "1rem",
             background:
               "linear-gradient(160.16deg, #BA6BFF -29.47%, #6C10BC 39.76%, #1E1E1E 101.6%)",
             borderRadius: "20px",
           }}
+        >
+          <Card
+            width="100%"
+            data={homeScreenData.vedamVs.middleSideData}
+            containerStyle={{
+              paddingLeft: "40px",
+              paddingRight: "40px",
+              background:
+                "linear-gradient(160.16deg, #BA6BFF -29.47%, #6C10BC 39.76%, #1E1E1E 101.6%)",
+              borderRadius: "20px",
+            }}
+            textArrayTextStyle={{
+              color: "rgba(255, 255, 255, 1)",
+              textAlign: "left",
+            }}
+          />
+        </Box>
+        <Card
+          data={homeScreenData.vedamVs.rightSideData}
+          containerStyle={{
+            display: { xs: "none", md: "flex" },
+            flex: "0 0 30%",
+            borderTop: "1px solid rgba(108, 16, 188, 1)",
+            borderRight: "1px solid rgba(108, 16, 188, 1)",
+            borderBottom: "1px solid rgba(108, 16, 188, 1)",
+            borderTopRightRadius: "20px",
+            borderBottomRightRadius: "20px",
+            paddingTop: "20px",
+            paddingBottom: "20px",
+            marginTop: "1rem",
+          }}
+          textArrayStyle={{
+            width: "100%",
+            paddingLeft: "40px",
+            justifyContent: "flex-start",
+          }}
+          headerTextStyle={{ color: "rgba(12, 38, 81, 1)" }}
           textArrayTextStyle={{
-            color: "rgba(255, 255, 255, 1)",
-            textAlign: "left",
+            color: "rgba(132, 132, 132, 1)",
           }}
         />
       </Box>
-      <Card
-        data={homeScreenData.vedamVs.rightSideData}
-        containerStyle={{
-          flex: { xs: "0 0 60%", md: "0 0 30%" },
-          borderTop: "1px solid rgba(108, 16, 188, 1)",
-          borderRight: "1px solid rgba(108, 16, 188, 1)",
-          borderBottom: "1px solid rgba(108, 16, 188, 1)",
-          borderTopRightRadius: "20px",
-          borderBottomRightRadius: "20px",
-          paddingTop: "20px",
-          paddingBottom: "20px",
-          marginTop: "1rem",
+
+      {/* Right Scroll Button - Only visible on mobile */}
+      <IconButton
+        onClick={scrollRight}
+        sx={{
+          display: { xs: "flex", md: "none" },
+          position: "absolute",
+          right: 0,
+          top: "50%",
+          transform: "translateY(-50%)",
+          zIndex: 100,
+          opacity: 0.8,
+          backgroundColor: "white",
+          "&:hover": {
+            backgroundColor: "white",
+          },
         }}
-        textArrayStyle={{
-          width: "100%",
-          paddingLeft: { xs: "20px", md: "40px" },
-          justifyContent: "flex-start",
-        }}
-        headerTextStyle={{ color: "rgba(12, 38, 81, 1)" }}
-        textArrayTextStyle={{
-          color: "rgba(132, 132, 132, 1)",
-        }}
-      />
+      >
+        <ChevronRightIcon />
+      </IconButton>
     </Box>
   );
 };
