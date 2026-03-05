f30d88558585d05ef59a8dbdf37b9d837f3559f5
SubheshVedam <subhesh@vedam.org>
2026-03-05 10:53:09 +0530

    changes 2

--- src/components/home/StudentsAtVedam.jsx
+++ src/components/home/StudentsAtVedam.jsx
@@ -48,7 +48,7 @@ export const StudentsAtVedam = () => {
                             }}
                         >
                             <Image
-                                src="/img/studentSuccess/krishiv_lfx_desktop_2.webp"
+                                src="/img/studentSuccess/krishiv_lfx_desktop.webp"
                                 alt="Student success story at Vedam"
                                 width={2560}
                                 height={846}
