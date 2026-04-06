6c0c886d027b8a98f58dd0fe615f6500718e2b47
SubheshVedam <subhesh@vedam.org>
2026-04-06 16:39:41 +0530

    Revert "Merge branch 'develop'"
    This reverts commit 387c475f15ece4a24f3ef282f6e81f5667db0f23, reversing
    changes made to ac3bcbb3a6e3c7387b3482c00f3aa558a6407329.

--- src/app/home/client/Home.jsx
+++ src/app/home/client/Home.jsx
@@ -222,7 +222,7 @@ const Home = () => {
           title={homeScreenData.hero.title}
           subtitle={homeScreenData.hero.subtitle}
           videoUrl={homeScreenData.hero.background_video}
-          showPosterImage={false}
+          posterImageUrl="/img/hear_from_founder_thumbnail.webp"
         />
         <SectionStack
           widthContainerProps={{
